cf36667ce1f300973f8582717dadea07fffc6a29
Rudra Makwana <rd851601@dal.ca>
2020-06-18 17:29:26 -0300

    Update QuestionDBMock.java

--- src/test/java/CSCI5308/GroupFormationTool/QuestionTest/QuestionDBMock.java
+++ src/test/java/CSCI5308/GroupFormationTool/QuestionTest/QuestionDBMock.java
@@ -1,79 +1,117 @@
 package CSCI5308.GroupFormationTool.QuestionTest;
 import CSCI5308.GroupFormationTool.Question.IQuestionPersistence;
 import CSCI5308.GroupFormationTool.Question.Question;
-import java.util.ArrayList;
-import java.util.List;
-
-public class QuestionDBMock implements IQuestionPersistence {
-
-	 public List<Question> loadAllQuestion() {
-
-	        List<Question> questionList = new ArrayList<>();
-	        Question question = new Question();
-	        question.setQuestionID(0);
-	        question.setQuestionTitle("Temp");
-	        question.setQuestionText("Test");
-	        question.setTypeID(1);
-	        questionList.add(question);
-	        question=new Question();
-	        question.setQuestionID(1);
-	        question.setQuestionTitle("Temp2");
-	        question.setQuestionText("Test2");
-	        question.setTypeID(2);
-	        questionList.add(question);
-	        return questionList;
-	    }
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 
-	    public void loadQuestionByID(long id, Question question) {
-	        question.setQuestionID(id);
-	        question.setQuestionTitle("Temp");
-	        question.setQuestionText("Test");
-	        question.setTypeID(1);
-	    }
-
-
-	    public boolean createQuestion(Question question) {
-	        question.setQuestionID(0);
-	        question.setQuestionTitle("Temp");
-	        question.setQuestionText("Test");
-	        question.setTypeID(1);
-	        return true;
-	    }
-
-
-	    public boolean deleteQuestion(long id)
-	    {
-	        Question question=new Question();
-	        question.setQuestionID(0);
-	        question.setQuestionTitle("Temp");
-	        question.setQuestionText("Test");
-	        question.setTypeID(1);
-	        question.setDefaults();
-	        return true;
-	    }
-
-	
-
-   
-    public List<Question> loadAllQuestionfromDB() {
-        return null;
+@SpringBootTest
+@SuppressWarnings("deprecation")
+class QuestionTest
+{
+    @Test
+    public void ConstructorTests()
+    {
+        Question question=new Question();
+        assertTrue(question.getQuestionID() == -1);
+        assertTrue(question.getQuestionTitle().isEmpty());
+        assertTrue(question.getQuestionText().isEmpty());
+        assertTrue(question.getTypeID()== -1);
     }
 
-   
-
-    public List<Question> loadQuestionByInstID(long id) {
-        return null;
-
-    
+    @Test
+    public void setIdTest()
+    {
+        Question question=new Question();
+        question.setQuestionID(7);
+        assertTrue(question.getQuestionID() == 7);
     }
-    public List<Question> loadQuestionByQID(long id) {
-        return null;
+
+    @Test
+    public void getIdTest()
+    {
+        Question question=new Question();
+        question.setQuestionID(7);
+        assertTrue(question.getQuestionID() == 7);
+    }
+
+    @Test
+    public void setTitleTest()
+    {
+        Question question=new Question();
+        question.setQuestionTitle("Temp");
+        assertTrue(question.getQuestionTitle().equals("Temp"));
 
     }
 
-    public boolean deleteQuestionById(long id) {
-        return false;
+    @Test
+    public void getTitleTest()
+    {
+        Question question=new Question();
+        question.setQuestionTitle("Temp");
+        assertTrue(question.getQuestionTitle().equals("Temp"));
+    }
+
+    @Test
+    public void setTextTest()
+    {
+        Question question=new Question();
+        question.setQuestionText("Test");
+        assertTrue(question.getQuestionText().equals("Test"));
+    }
+
+
+    @Test
+    public void getTextTest()
+    {
+        Question question=new Question();
+        question.setQuestionTitle("Test");
+        assertTrue(question.getQuestionTitle().equals("Test"));
+    }
+
+    @Test
+    public void getTypeIDTest()
+    {
+        Question question=new Question();
+        question.setTypeID(0);
+        assertTrue(question.getTypeID() == 0);
+    }
+
+    @Test
+    public void setTypeIDTest()
+    {
+        Question question=new Question();
+        question.setTypeID(0);
+        assertTrue(question.getTypeID() == 0);
+    }
+
+    @Test
+    public void deleteQuestionTest()
+    {
+        IQuestionPersistence questionDB=new QuestionDBMock();
+        boolean status=questionDB.deleteQuestion(0);
+        assertTrue(status);
+
+    }
+
+    @Test
+    public void createQuestionTest()
+    {
+        IQuestionPersistence questionDB=new QuestionDBMock();
+        Question question=new Question();
+        question.setQuestionID(0);
+        question.setQuestionTitle("Temp");
+        question.setQuestionText("Test");
+        question.setTypeID(1);
+        questionDB.createQuestion(question);
+        assertTrue(question.getQuestionID()==0);
+        assertTrue(question.getQuestionTitle().equals("Temp"));
+        assertTrue(question.getQuestionText().equals("Test"));
+        assertTrue(question.getTypeID()==1);
+
+    }
+
 }
-    
-}
+
+
