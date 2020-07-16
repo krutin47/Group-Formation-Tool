5209f03934bbe56c064a7f2a5572db812b94f83d
Chetanpreet Singh Sachdeva <ch769380@dal.ca>
2020-07-16 20:26:28 -0300

    Cleaned up Code

--- src/main/java/CSCI5308/GroupFormationTool/Answers/CheckSurveyController.java
+++ src/main/java/CSCI5308/GroupFormationTool/Answers/CheckSurveyController.java
@@ -1,5 +1,7 @@
 package CSCI5308.GroupFormationTool.Answers;
 
+import CSCI5308.GroupFormationTool.Questions.Choice;
+import CSCI5308.GroupFormationTool.Questions.IChoice;
 import CSCI5308.GroupFormationTool.Questions.IQuestion;
 import CSCI5308.GroupFormationTool.Questions.Question;
 import org.springframework.stereotype.Controller;
@@ -64,26 +66,21 @@ public class CheckSurveyController {
 
 
     //The survey page
-    @GetMapping("/survey/survey/question")
+    @GetMapping("/survey/survey")
     public String Survey(Model model)
     {
 
         IStudentSurvey studentSurvey=SystemConfig.instance().getStudentSurvey();
         IQuestion q=SystemConfig.instance().getQuestionService();
-
-        List<Question> questions = new ArrayList<>();      //list to contain all questions linked to survey
-
         surveyid = studentSurvey.getSurveyIDfromCourse(storeval);    //load Survey ID by course ID
-
         List<Long> questionID = q.loadQuestionIDbySurveyID(surveyid);   //Load list of question ID's linked to survey
-
-        for(long l:questionID)
+        List<Question> questions = new ArrayList<>();      //list to contain all questions linked to survey
+        for(long l:questionID)     //Iterate over different question id's
         {
           questions.add(q.loadQuestionbyID(l));  //Store all question objects of one surveyID,in one list
         }
         //PASS: serveyID to view and then fetch it.
         model.addAttribute("questions",questions);
-
 
 
         for(Question qt:questions)  //checking the type of question
@@ -110,91 +107,5 @@ public class CheckSurveyController {
         return "/survey/survey";
     }
 
-    /**
-     *  [
-     *      {
-     *          questionID:value,
-     *          questionTitle:value,
-     *          questionText:value
-     *          Answer:{
-     *              questionType:Number|text|multiple|checkbox,
-     *              multiple:[//load from db(multiple choice answers)]
-     *          },
-     *
-     *      },
-     *      {
-     *
-     *      },
-     *      {
-     *
-     *      }
-     *  ]
-     * */
 
-    //store number answer
-    @RequestMapping(value="/survey/survey/addAnswerNumber", method = RequestMethod.POST)
-    public String addAnswerNumber(@RequestParam(name = ANSWER)String s,Model model,@RequestParam(name = ID) Long QuestionID)
-    {
-        IAnswer answerDB= SystemConfig.instance().getAnswerDB();
-        User u=new User();
-        u=CurrentUser.instance().getCurrentAuthenticatedUser();
-        Long userID;
-        userID=u.getID();   //user ID
-        Answer ans=new Answer();
-        ans.setAnswerValue(s); //store the text answer
-        answerDB.createAnswer(ans,surveyid,QuestionID,userID);
-                return "/survey/survey";
-    }
-
-
-
-    //store Multiple choice, choose one
-    @RequestMapping(value="/survey/survey/addAnswerMul1", method = RequestMethod.POST)
-    public String addAnswerMul1(@RequestParam(name = ANSWER)String s,Model model,@RequestParam(name = ID) Long QuestionID)
-    {
-        IAnswer answerDB= SystemConfig.instance().getAnswerDB();
-        User u=new User();
-        u=CurrentUser.instance().getCurrentAuthenticatedUser();
-        Long userID;
-        userID=u.getID();   //user ID
-        Answer ans=new Answer();
-        ans.setAnswerValue(s); //store the text answer
-        answerDB.createAnswer(ans,surveyid,QuestionID,userID);
-        return "/survey/survey";
-    }
-
-
-
-
-    //store Multiple choice, choose multiple
-    @RequestMapping(value="/survey/survey/addAnswerMul2", method = RequestMethod.POST)
-    public String addAnswerMul2(@RequestParam(name = ANSWER)String s,Model model,@RequestParam(name = ID) Long QuestionID)
-    {
-        IAnswer answerDB= SystemConfig.instance().getAnswerDB();
-        User u=new User();
-        u=CurrentUser.instance().getCurrentAuthenticatedUser();
-        Long userID;
-        userID=u.getID();   //user ID
-        Answer ans=new Answer();
-        ans.setAnswerValue(s); //store the text answer
-        answerDB.createAnswer(ans,surveyid,QuestionID,userID);
-        return "/survey/survey";
-    }
-
-
-
-    //store Free text
-    @RequestMapping(value="/survey/survey/addAnswerFree", method = RequestMethod.POST)
-    public String addAnswerFree(@RequestParam(name = ANSWER)String s,Model model,@RequestParam(name = ID) Long QuestionID)
-    {
-        IAnswer answerDB= SystemConfig.instance().getAnswerDB();
-        User u=new User();
-        u=CurrentUser.instance().getCurrentAuthenticatedUser();
-        Long userID;
-        userID=u.getID();   //Current user ID
-        Answer ans=new Answer();
-        ans.setAnswerValue(s); //store the text answer
-        answerDB.createAnswer(ans,surveyid,QuestionID,userID);
-        return "/survey/survey";
-    }
 }
