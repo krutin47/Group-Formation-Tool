2d25325cadbaae7fdf0ad52a64c97263ae110e11
Japnoor Kaur <jp238553@dal.ca>
2020-07-16 18:56:26 -0300

    Update QuestionDBMock.java

--- src/test/java/CSCI5308/GroupFormationTool/QuestionsTest/QuestionDBMock.java
+++ src/test/java/CSCI5308/GroupFormationTool/QuestionsTest/QuestionDBMock.java
@@ -69,4 +69,37 @@ public class QuestionDBMock implements IQuestion {
         question = new Question();
         return true;
     }
+     @Override
+    public Question loadQuestionbyQid(long id)
+    {
+        Question question = new Question();
+        question.setQuestionID(0);
+        question.setQuestionTitle("Temp");
+        question.setQuestionText("Test");
+        question.setTypeID(1);
+        question=new Question();
+        question.setQuestionID(1);
+        question.setQuestionTitle("Temp2");
+        question.setQuestionText("Test2");
+        question.setTypeID(2);
+        return question;
+    }
+    public List<Question> loadInstructorQuestionsList(long id) {
+        List<Question> questionList = new ArrayList<>();
+        Question question = new Question();
+        User user=new User();
+        user.setID(22);
+        question.setQuestionID(0);
+        question.setQuestionTitle("Temp");
+        question.setQuestionText("Test");
+        question.setTypeID(1);
+        questionList.add(question);
+        question=new Question();
+        question.setQuestionID(1);
+        question.setQuestionTitle("Temp2");
+        question.setQuestionText("Test2");
+        question.setTypeID(2);
+        questionList.add(question);
+        return questionList;
+    }
 }
