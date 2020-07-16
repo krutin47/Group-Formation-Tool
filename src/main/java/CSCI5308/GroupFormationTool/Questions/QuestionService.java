4153cff8e1b9b596ae532c10396711c6cb2c77d6
Japnoor Kaur <jp238553@dal.ca>
2020-07-16 20:32:47 -0300

    Update QuestionService.java

--- src/main/java/CSCI5308/GroupFormationTool/Questions/QuestionService.java
+++ src/main/java/CSCI5308/GroupFormationTool/Questions/QuestionService.java
@@ -1,7 +1,5 @@
 package CSCI5308.GroupFormationTool.Questions;
 
-import CSCI5308.GroupFormationTool.Courses.Course;
-import CSCI5308.GroupFormationTool.Database.CallQuery;
 import CSCI5308.GroupFormationTool.Database.CallStoredProcedure;
 
 import java.sql.Date;
@@ -64,157 +62,108 @@ public class QuestionService implements IQuestion {
         return true;
     }
 
-   
     @Override
-    public List<Question> loadQuestionByInstID(long id)
-    {
-        List<Question> questions = new ArrayList<Question>();
-        CallQuery callquery = null;
-        String query2="Select questionID, questionTitle, questionText, creationDate\r\n" +
-                "FROM Question \r\n" +
-                "where questionID IN (Select questionID from InstructorQuestionMapper where instructorID='"+id+"');";
+	public List<Question> loadQuestionByInstID(long id)
+	{
+		List<Question> questions = new ArrayList<Question>();
+		CallStoredProcedure proc = null;
+		try
+		{
+			proc = new CallStoredProcedure("sploadQuestionByInstID(?)");
+			proc.setParameter(1, id);
 
-        try
-        {
-            callquery = new CallQuery(query2);
+			ResultSet results = proc.executeWithResults();
+			if (null != results)
+			{
+				while (results.next())
+				{
+					long questionID = results.getLong(1);
+					String questionTitle = results.getString(2);
+					String questionText = results.getString(3);
+					Date creationDate = results.getDate(4);
 
-            ResultSet results = callquery.executeWithResults(query2);
-            if (null != results)
-            {
-                while (results.next())
-                {
+					Question q = new Question();
+					q.setQuestionID(questionID);
+					q.setQuestionTitle(questionTitle);
+					q.setQuestionText(questionText);
+					q.setCreationDate(creationDate);
+					questions.add(q);
+				}
+			}
+		}
+		catch (SQLException e)
+		{
+			// Logging needed.
+		}
+		finally
+		{
+			if (null != proc)
+			{
+				proc.cleanup();
+			}
+		}
+		return questions;
+	}
 
-
-                    long qid =results.getLong("questionID");
-                    String title = results.getString("questionTitle");
-                    String text = results.getString("questionText");
-                    Date date=results.getDate("creationDate");
-                    Question q = new Question();
-                    q.setQuestionID(qid);
-                    q.setQuestionTitle(title);
-                    q.setQuestionText(text);
-                    q.setCreationDate(date);
-
-                    questions.add(q);
-
-                }
-            }
-        }
-        catch (SQLException e)
-        {
-            // Logging needed.
-        }
-        finally
-        {
-            if (null != callquery)
-            {
-                callquery.cleanup();
-            }
-        }
-        return questions;   //returns the list of questions found
-
-
-    }
-
+ 
     @Override
-    public List<Question> loadQuestionByQID(long id)
-    {
-        List<Question> questions = new ArrayList<Question>();
-        CallQuery callquery = null;
-        String query2="Select questionID, questionTitle, questionText, creationDate\r\n" +
-                "FROM Question \r\n" +
-                "where questionID='"+id+"';";
+   	public List<Question> loadQuestionByQID(long id)
+   	{
+   		List<Question> questions = new ArrayList<Question>();
+   		CallStoredProcedure proc = null;
+   		try
+   		{
+   			proc = new CallStoredProcedure("sploadQuestionByQuestionID(?)");
+   			proc.setParameter(1, id);
 
-        try
-        {
-            callquery = new CallQuery(query2);
+   			ResultSet results = proc.executeWithResults();
+   			if (null != results)
+   			{
+   				while (results.next())
+   				{
+   					long questionID = results.getLong(1);
+   					String questionTitle = results.getString(2);
+   					String questionText = results.getString(3);
+   					Date creationDate = results.getDate(4);
 
-            ResultSet results = callquery.executeWithResults(query2);
-            if (null != results)
-            {
-                while (results.next())
-                {
-
-
-                    long qid =results.getLong("questionID");
-                    String title = results.getString("questionTitle");
-                    String text = results.getString("questionText");
-                    Date date=results.getDate("creationDate");
-                    Question q = new Question();
-                    q.setQuestionID(qid);
-                    q.setQuestionTitle(title);
-                    q.setQuestionText(text);
-                    q.setCreationDate(date);
-
-                    questions.add(q);
-
-                }
-            }
-        }
-        catch (SQLException e)
-        {
-            // Logging needed.
-        }
-        finally
-        {
-            if (null != callquery)
-            {
-                callquery.cleanup();
-            }
-        }
-        return questions;   //returns the list of questions found
-    }
-
+   					Question q = new Question();
+   					q.setQuestionID(questionID);
+   					q.setQuestionTitle(questionTitle);
+   					q.setQuestionText(questionText);
+   					q.setCreationDate(creationDate);
+   					questions.add(q);
+   				}
+   			}
+   		}
+   		catch (SQLException e)
+   		{
+   			// Logging needed.
+   		}
+   		finally
+   		{
+   			if (null != proc)
+   			{
+   				proc.cleanup();
+   			}
+   		}
+   		return questions;
+   	}
+    
     @Override
-    public boolean deleteQuestionById(long id)
-    {
-        CallQuery callquery1 = null;
-        CallQuery callquery2 = null;
-
-        CallQuery callquery3 = null;
-        CallQuery callquery4 = null;
-
-        String query3=" DELETE FROM Question\r\n" +
-                "    WHERE Question.questionID = '"+id+"';";
-        String query1="DELETE FROM QuestionOption\r\n" +
-                "    WHERE QuestionOption.questionID = '"+id+"';";
-        String query2="DELETE FROM QuestionTypeMapper\r\n" +
-                "    WHERE QuestionTypeMapper.questionID = '"+id+"';";
-        String query4="DELETE FROM InstructorQuestionMapper\r\n" +
-                "    WHERE InstructorQuestionMapper.questionID = '"+id+"';";
+    public boolean deleteQuestionById(long id) {
+        CallStoredProcedure callStoredProcedure = null;
         try{
-            callquery4 = new CallQuery(query4);
-            callquery4.executeUpdate(query4);
-
-            callquery2 = new CallQuery(query2);
-            callquery2.executeUpdate(query2);
-
-            callquery1 = new CallQuery(query1);
-            callquery1.executeUpdate(query1);
-
-            callquery3 = new CallQuery(query3);
-            System.out.println("query is="+query3);
-
-            callquery3.executeUpdate(query3);
-
-        }catch (SQLException e) {
-            // Logging needed
+            callStoredProcedure = new CallStoredProcedure("spDeleteQuestionById(?)");
+            callStoredProcedure.setParameter(1, id);
+            
+            callStoredProcedure.execute();
+        }catch (SQLException e){
+            e.printStackTrace();
             return false;
         }finally {
-            if (null != callquery1) {
-                callquery1.cleanup();
-            }
-
-            if (null != callquery2) {
-                callquery2.cleanup();
-            }
-
-            if (null != callquery3) {
-                callquery3.cleanup();
-            }
-
-            if (null != callquery4) {
-                callquery4.cleanup();
+            if (null != callStoredProcedure)
+            {
+                callStoredProcedure.cleanup();
             }
         }
         return true;
